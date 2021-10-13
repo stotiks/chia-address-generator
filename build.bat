bcf4c64f7f6f84006da8c300f9829115f1b5e695
stotiks <47013208+stotiks@users.noreply.github.com>
2021-10-13 15:53:53 +0300

    Delete build.bat

--- build.bat
+++
@@ -1,4 +0,0 @@
-pyinstaller --onefile create_address.py ^
---add-data=".\base\wallet\puzzles\p2_conditions.clvm.hex;base\wallet\puzzles" ^
---add-data=".\base\wallet\puzzles\calculate_synthetic_public_key.clvm.hex;base\wallet\puzzles" ^
---add-data=".\base\wallet\puzzles\p2_delegated_puzzle_or_hidden_puzzle.clvm.hex;base\wallet\puzzles"
